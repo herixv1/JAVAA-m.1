a1219cf06945a67b1a5f128be4b15a1d11bcf7af
Herixv1 <herymimo11@gmail.com>
2026-04-23 22:37:14 -0700

    Fin 4.1&4.2

--- src/INT1/PartidoPerron.java
+++ src/INT1/PartidoPerron.java
@@ -1,23 +1,25 @@
 package INT1;
 
-import java.util.ArrayList;
-
 public class PartidoPerron {
 
-    private ArrayList<EnviadorMensaje> Mensajeros;
+    private EnviadorMensaje[] Mensajeros;
+    private int cont;
 
     public PartidoPerron() {
-        Mensajeros = new ArrayList<>();
+        Mensajeros = new EnviadorMensaje[10]; 
+        cont = 0;
     }
 
     public void agregarMensajero(EnviadorMensaje mensajero) {
-        Mensajeros.add(mensajero);
+        Mensajeros[cont] = mensajero;
+        cont++;
     }
 
-    public void hacerCampana(){
-        for (EnviadorMensaje mensajero : Mensajeros) {
-            mensajero.enviarMensaje("Vota por el partido perron");
+    public void hacerCampana() {
+
+        for (int i = 0; i < cont; i++) {
+            Mensajeros[i].enviarMensaje("Vota por el partido perron");
         }
     }
-
 }
+
--- src/INT2/Apepe.java
+++ src/INT2/Apepe.java
@@ -1,5 +1,32 @@
 package INT2;
 
 public class Apepe {
+    public static void main(String[] args) {
+        
+        // 1. demostracion de herencia
+        Arania miArania = new Arania();
+        miArania.caminar(); 
 
+        // 2. demostracion de abstraccion e interfaces
+        Mascota mascota1 = new Gatou("Messi");
+        Mascota mascota2 = new Pescadou();
+        mascota2.setNombre("nemo");
+
+        mascota1.jugar(); 
+        mascota2.jugar(); 
+
+        // 3. demostracion de polimorfismo
+        Animal[] zoologico = new Animal[] {
+            new Gatou("chorizo"),
+            new Arania(),
+            new Pescadou()
+        };
+
+        for (int i = 0; i < zoologico.length; i++) {
+            zoologico[i].comer();
+            zoologico[i].caminar();
+        }
+    }
 }
+
+
